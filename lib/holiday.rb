0f0aa37882709a5fa0f33c3956096305b5439a8c
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-20 00:34:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, data|
     puts "#{season.capitalize}:"
       holiday.each do |name, supply|
-        puts "  #{name.to_s.split("_").each {|x| x.capitalize!}.join(" ")}: #{supply}"
+        puts "  #{name.to_s.split("_").each {|x| x.capitalize!}.join(" ")}: #{supply.each }"
 end
 
 def all_holidays_with_bbq(holiday_hash)
