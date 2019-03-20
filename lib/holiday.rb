7d43b127025b3e58de661dd0be52df0e317973fb
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-20 00:39:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  new_array = []
+  holiday_hash.each do |season, |
 end
 
 
